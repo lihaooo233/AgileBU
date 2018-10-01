85a09e9715b59ae68ca5607b7ce8b001e05cc2cc
Ghukek <nwiebe@bu.edu>
2018-10-01 17:23:57 -0400

    Update

--- agilebu/src/RMenu/RMenu.js
+++ agilebu/src/RMenu/RMenu.js
@@ -1,23 +1,52 @@
-import React, { Component } from 'react'
-import './RMenu.css'
+import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardTitle from './CardTitle.js'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = ({
+    sidebar: {
+        position:'absolute',
+        top:40, bottom:0, right:0,
+        width:300,
+        background:'#E6E6E6',
+    },
+    grid: {
+        container:true,
+        direction:"column",
+        justify:"flex-start",
+        alignItems:"stretch",
+        wrap:"nowrap",
+        position:'absolute',
+        top:0, bottom:0, right:0,
+        background:'#E6E6E6',
+    },
+    card: {
+        background: '#848484',
+        height: '40%',
+    },
+});
 
+function RMenu(props) {
+    const { classes } = props;
 
-export default class RMenu extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <sidebar className="App-sidebar">
-                    <Grid className="App-grid">
-                        <CardTitle />
-                        <Card className="App-chatbox">
-                        </Card>
-                    </Grid>
-                </sidebar>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <sidebar className={classes.sidebar}>
+                <Grid className={classes.grid}>
+                    <CardTitle />
+                    <Card className={classes.card}>
+                        DiscussionBoard
+                    </Card>
+                </Grid>
+            </sidebar>
+        </div>
+    )
 }
+
+RMenu.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RMenu)
